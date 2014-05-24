4f7a7ec40d834aa10a3879c8006676b789fb0eac
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-24 13:36:43 +0200

    Update Array-Consume.js

--- Array-Consume.js
+++ Array-Consume.js
@@ -1,3 +1,12 @@
+(function(e,p,f){
+ e.Consume=f[p]({p:'pop'});
+ e.ConsumeTopDown=f[p]({p:'shift'});
+})(Array,'bind',function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
+
+
+//...
+
+
 (function(e,f){
  e.Consume=f.bind({p:'pop'});
  e.ConsumeTopDown=f.bind({p:'shift'});
