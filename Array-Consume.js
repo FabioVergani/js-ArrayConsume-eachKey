aea45976f0199f6c64f8ef8bce32146ae6dd00b6
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-24 13:46:19 +0200

    Update Array-Consume.js

--- Array-Consume.js
+++ Array-Consume.js
@@ -1,20 +1,32 @@
+
+
+//147
+(function(e,f){
+ e.Consume=f.bind({p:'pop'});
+ e.ConsumeTopDown=f.bind({p:'shift'});
+})(Array,function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
+
+
+//152
+(function(i,e,f){
+ e[i]=f.bind({p:'pop'});
+ e[i+'TopDown']=f.bind({p:'shift'});
+})('Consume',Array,function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
+//152
 (function(e,p,f){
  e.Consume=f[p]({p:'pop'});
  e.ConsumeTopDown=f[p]({p:'shift'});
 })(Array,'bind',function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
 
+//157
+(function(i,e,p,f){
+ e[i]=f[p]({p:'pop'});
+ e[i+'TopDown']=f[p]({p:'shift'});
+})('Consume',Array,'bind',function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
 
-//...
 
-
-(function(e,f){
- e.Consume=f.bind({p:'pop'});
- e.ConsumeTopDown=f.bind({p:'shift'});
-})(Array,function(o,f){var e,p=this.p;while(e=o[p]()){f(e)}});
-//-------------------------------------------------------------------------------------------------------
 /*
 function Test(x){console.log(x)};
 Array.Consume(['a','b','c'],Test);//c-b-a
 Array.ConsumeTopDown(['a','b','c'],Test);//a-b-c
 */
-
